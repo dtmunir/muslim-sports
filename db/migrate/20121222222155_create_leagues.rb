9b2936ef2969fb2188b70c052fe3a7235937f100
Danish Munir <dtmunir@gmail.com>
2012-12-22 17:23:41 -0500

    Added league model

---
+++ db/migrate/20121222222155_create_leagues.rb
@@ -0,0 +1,11 @@
+class CreateLeagues < ActiveRecord::Migration
+  def change
+    create_table :leagues do |t|
+      t.string :name
+      t.string :sport
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
