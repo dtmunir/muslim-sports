ff1e560f628c10ff92afbc644a64862e3e1d357d
Danish Munir <dtmunir@gmail.com>
2012-12-22 14:33:36 -0500

    created home page with about us and contact links and related tests

--- config/routes.rb
+++ config/routes.rb
@@ -53,8 +53,8 @@ MuslimSports::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # just rememberrake d to delete public/index.html.
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
