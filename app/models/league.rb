ea1f3969e824057df9fece38f7b45d594f8f4504
Danish Munir <dtmunir@gmail.com>
2012-12-24 20:11:56 -0500

    Completed the New League creation form. We dont know if it passes the tests yet

--- app/models/league.rb
+++ app/models/league.rb
@@ -14,4 +14,9 @@ class League < ActiveRecord::Base
   attr_accessible :description, :name, :sport
 
   has_and_belongs_to_many :users
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :sport, :presence => true
+
+
 end
